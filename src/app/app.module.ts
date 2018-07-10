bfb622f7b31abddf6f64462b7b7c572592193fce
Francisco C <javier_cansa@hotmail.com>
2018-07-09 23:19:35 -0500

    **FJCS**

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import {HttpClientModule} from '@angular/common/http';
 // Rutas
 import { ROUTES } from './app.routes';
+import { NoimagePipe } from './pipes/noimage.pipe';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ROUTES } from './app.routes';
     HomeComponent,
     SearchComponent,
     AtirstComponent,
-    NavbarComponent
+    NavbarComponent,
+    NoimagePipe
   ],
   imports: [
     HttpClientModule,
